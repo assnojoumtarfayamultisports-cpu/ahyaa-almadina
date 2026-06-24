d9f12b16330b511ec293931ea50c934b0c8eb9b9
assnojoumtarfayamultisports-cpu <ass.nojoum.tarfaya.multisports@gmail.com>
2026-06-24 15:30:30 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+alert("APP STARTED");
+
+document.getElementById("teams").innerHTML =
+  "<h3>JavaScript يعمل بنجاح</h3>";
+
+document.getElementById("matches").innerHTML =
+  "<h3>قسم المباريات يعمل</h3>";
+
+document.getElementById("news").innerHTML =
+  "<h3>قسم الأخبار يعمل</h3>";
